188ee194eb586dd205aab4cf47695d40d90909a0
Marcin <klasicki.m@gmail.com>
2020-08-04 16:45:20 +0200

    Added thymeleaf view "demo" witch consumes rest api

--- src/main/java/pl/klasicki/controllers/DemoController.java
+++ src/main/java/pl/klasicki/controllers/DemoController.java
@@ -1,59 +1,37 @@
 package pl.klasicki.controllers;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-import pl.klasicki.domain.Doctor;
-
-
-import java.util.HashMap;
-import java.util.Map;
+import pl.klasicki.services.DemoControllerService;
 
 
 @Controller
 @RequestMapping("/demo")
 public class DemoController {
 
-    private RestTemplate restTemplate;
-    private final String BASE_URL = "http://localhost:8080/api/doctors/";
-    private final String URL_WITH_ID_PARAM = "http://localhost:8080/api/doctors/{id}";
+    private final Logger logger = LoggerFactory.getLogger(DemoController.class);
+    private DemoControllerService demoControllerService;
 
-    public DemoController(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+   // private final String URL_WITH_ID_PARAM = "http://localhost:8080/api/doctors/id/{id}";
+
+    public DemoController(DemoControllerService demoControllerService) {
+        this.demoControllerService = demoControllerService;
     }
 
     @GetMapping("/doctors")
     public String getDoctors(Model model) {
 
-        model.addAttribute("doctors", restTemplate.getForObject(BASE_URL, Object[].class));
-        return "demo";
+        model.addAttribute("doctors", demoControllerService.getAll());
 
-    }
-
-
-    @GetMapping("/doctors/{id}")
-    public String findById(@PathVariable Long id, Model model) {
-        Map<String, Long> param = new HashMap<>();
-        param.put("id", id);
-        ResponseEntity<Doctor> response = restTemplate.exchange(URL_WITH_ID_PARAM,
-                                                                HttpMethod.GET,
-                                                                HttpEntity.EMPTY,
-                                                                Doctor.class, param);
-        model.addAttribute("doctor", response);
+        logger.info("Calling method from DemoController class");
 
         return "demo";
+
     }
 
-    @DeleteMapping("/doctors/{id}")
-    public void delete(@PathVariable Long id) {
-        String url = "http://localhost:8080/api/doctors/" + id;
-        restTemplate.delete(url);
-        System.out.println("Skasowano encję o id " + id);
-    }
 
 
 }
---
+++ src/main/java/pl/klasicki/services/DemoControllerService.java
@@ -0,0 +1,11 @@
+package pl.klasicki.services;
+
+import pl.klasicki.domain.Doctor;
+
+import java.util.List;
+
+public interface DemoControllerService {
+
+  List<Doctor> getAll();
+
+}
