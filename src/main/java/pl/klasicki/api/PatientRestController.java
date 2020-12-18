4cfc57e5fc324cadb6162c5bd583a28f879d2c66
Marcin <klasicki.m@gmail.com>
2020-12-18 12:37:36 +0100

    Changes response type in PatientRestController, used ResponseEntity.

--- src/main/java/pl/klasicki/api/PatientRestController.java
+++ src/main/java/pl/klasicki/api/PatientRestController.java
@@ -2,6 +2,8 @@ package pl.klasicki.api;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.klasicki.domain.Patient;
 import pl.klasicki.exceptions.PatientNotFoundException;
@@ -22,37 +24,43 @@ public class PatientRestController {
 
     @ApiOperation(value = "Get list of all the patients")
     @GetMapping("/api/patients/")
-    public List<Patient> getAll() {
-        return patientService.getAll();
+    public ResponseEntity<List<Patient>> getAll() {
+        return new ResponseEntity<>(patientService.getAll(), HttpStatus.OK);
     }
 
     @ApiOperation(value = "Get patient by id")
     @GetMapping("/api/patients/{id}")
-    public Optional<Patient> findById(@PathVariable Long id) {
-
-        if (id < 0 || id > patientService.getAll().size()) {
-            throw new PatientNotFoundException("Patient with id " + id + " not found.");
-        }
-
-        return patientService.findById(id);
+    public ResponseEntity<Optional<Patient>> findById(@PathVariable Long id) {
+        requestCheck(id);
+        return new ResponseEntity<>(patientService.findById(id), HttpStatus.OK);
     }
 
     @ApiOperation(value = "Create a new patient")
     @PostMapping("/api/patients/")
-    public Patient add(@RequestBody Patient patient) {
-        return patientService.add(patient);
+    public ResponseEntity<Patient> add(@RequestBody Patient patient) {
+        return new ResponseEntity<>(patientService.add(patient),HttpStatus.CREATED);
     }
 
     @ApiOperation(value = "Update patient's details")
     @PutMapping("/api/patients/{id}")
-    public Patient update(@PathVariable Long id, @RequestBody Patient patient) {
-        return patientService.add(patient);
+    public ResponseEntity<Patient> update(@PathVariable Long id, @RequestBody Patient patient) {
+        requestCheck(id);
+        return new ResponseEntity<>(patientService.add(patient), HttpStatus.OK);
     }
 
     @ApiOperation("Delete patient")
     @DeleteMapping("/api/patients/{id}")
-    public void delete(@PathVariable Long id) {
+    public  ResponseEntity<?> delete(@PathVariable Long id) {
+        requestCheck(id);
         patientService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private void requestCheck(Long id) {
+        if (id < 0 || id > patientService.getAll().size()) {
+            throw new PatientNotFoundException("Patient with id " + id + " not found.");
+        }
     }
 
 }
+
