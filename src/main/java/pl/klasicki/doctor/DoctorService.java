8df0e33636b064306d2591c016c303c9ab11290c
Marcin <klasicki.m@gmail.com>
2021-12-20 23:32:39 +0100

    Made doctorService public as adapter for commands

--- src/main/java/pl/klasicki/doctor/DoctorService.java
+++ src/main/java/pl/klasicki/doctor/DoctorService.java
@@ -6,24 +6,24 @@ import org.springframework.stereotype.Service;
 
 
 @Service
-class DoctorService {
+public class DoctorService {
 
     private final DoctorRepository doctorRepository;
     private final DoctorMapper mapper;
     private Logger logger = LoggerFactory.getLogger(DoctorService.class);
 
-    DoctorService(DoctorRepository doctorRepository, DoctorMapper mapper)
+    public DoctorService(DoctorRepository doctorRepository, DoctorMapper mapper)
     {
         this.doctorRepository = doctorRepository;
         this.mapper = mapper;
     }
 
-    Doctor add(DoctorDto doctor) {
+    public Doctor add(DoctorDto doctor) {
         logger.info("Added new doctor to the doctor list with name {}", doctor.getFirstName());
         return doctorRepository.save(mapper.toDomain(doctor));
     }
 
-    void delete(Long id) {
+    public void delete(Long id) {
         logger.info("Deleting from list doctor with id {}", id);
         doctorRepository.deleteById(id);
     }
